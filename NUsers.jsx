27206bc9aa2d145818dd4729796cdee1dfe461f4
naren30024 <126703711+naren30024@users.noreply.github.com>
2023-12-20 18:16:41 +0530

    Update NUsers.jsx

--- NUsers.jsx
+++ NUsers.jsx
@@ -1,69 +1,82 @@
-import React,{useState,useEffect} from 'react'
-import style from './style.module.css'
+import React, { useState, useEffect } from 'react';
 import TopNavBar from './TopNavBar';
 
 export default function NUsers() {
     const [users, setUsers] = useState([]);
     const [showDetails, setShowDetails] = useState({});
+
     useEffect(() => {
-        async function fetchCountries() {
+        async function fetchUsers() {
+            const AT = JSON.parse(localStorage.getItem("at"));
             try {
-              const response = await fetch(`http://127.0.0.1:5000/api/users`);
-              const data = await response.json();
-              setUsers(data.data);
-              console.log(users)
+                const response = await fetch(`http://127.0.0.1:5000/api/users`, {
+                    method: 'GET',
+                    headers: {
+                        authorization: `Bearer ${AT}`,
+                    },
+                });
+                const data = await response.json();
+                setUsers(data.data);
             } catch (error) {
-              console.error('Error fetching countries:', error);
+                console.error('Error fetching users:', error);
             }
-          }
-          fetchCountries();
-        
+        }
+        fetchUsers();
     }, []);
+
     const toggleDetails = (id) => {
         setShowDetails((prevState) => ({
-          ...prevState,
-          [id]: !prevState[id],
+            ...prevState,
+            [id]: !prevState[id],
         }));
-      };
-    
-    
-  return (
-    <>
-     <div className={style.fixedtop}>
-        <TopNavBar/>
-     </div>
-     <div className={style.users}>
-        <h1>Voter List</h1>
-          {users.map((user) => (
-            <div key={user.id} style={{ marginBottom: '20px' }}>
-              <div style={{ display: 'flex', alignItems: 'center' }}>
-                <h3>{user.name}</h3>
-                <button onClick={() => toggleDetails(user.id)}>
-                  {showDetails[user.id] ? '▼' : '►'}
-                </button>
-              </div>
-              {showDetails[user.id] && (
-                <div style={{ marginTop: '10px', border: '1px solid #ccc', padding: '10px', width:"300px" }}>
-                  <p>Name : {user.name}</p>
-                  <p>father's name: {user.father_name}</p>
-                  <p>age: {user.age}</p>
-                  <p>gender: {user.gender}</p>
-                  <p>caste: {user.caste}</p>
-                  <p>religion: {user.religion}</p>
-                  <p>education: {user.education}</p>
-                  <p>employement: {user.employement}</p>
-                  <p>voter ID: {user.voter_id}</p>
-                  <p>booth no: {user.booth_no}</p>
-                  <p>prev voting date: {user.pre_voting_date}</p>
-                  <p>prev vote to: {user.pre_vote_to}</p>
-                  <p>membership: {user.membership}</p>
-                  <p>affiliation: {user.affiliation}</p>
-                  <p>influence factor: {user.influence_factor}</p>
-                </div>
-              )}
+    };
+
+    return (
+        <>
+            <div className="fixed top-0 w-full">
+                <TopNavBar />
             </div>
-            ))}
-      </div>
-    </>
-  )
+            <div className="flex justify-center mt-8">
+                <div className="w-full lg:w-2/3">
+                    <h1 className="text-3xl font-semibold text-center mb-6">Voter List</h1>
+                    <div className="flex flex-col gap-4">
+                        {users.map((user) => (
+                            <div key={user.id} className="border rounded-lg p-4">
+                                <div className="flex items-center justify-between mb-2">
+                                    <h3 className="text-xl">{user.name}</h3>
+                                    <button
+                                        onClick={() => toggleDetails(user.id)}
+                                        className="text-blue-500 hover:text-blue-700"
+                                    >
+                                        {showDetails[user.id] ? '▼' : '►'}
+                                    </button>
+                                </div>
+                                {showDetails[user.id] && (
+                                    <div className="border-t mt-4 pt-4">
+                                        <p><strong>Name:</strong> {user.name}</p>
+                                        <p><strong>Father's name:</strong> {user.father_name}</p>
+                                        <p><strong>age:</strong> {user.age}</p>
+                                        <p><strong>gender:</strong> {user.gender}</p>
+                                        <p><strong>caste:</strong> {user.caste}</p>
+                                        <p><strong>religion:</strong> {user.religion}</p>
+                                        <p><strong>education:</strong> {user.education}</p>
+                                        <p><strong>employement:</strong> {user.employement}</p>
+                                        <p><strong>voter ID:</strong> {user.voter_id}</p>
+                                        <p><strong>booth no:</strong> {user.booth_no}</p>
+                                        <p><strong>prev voting date:</strong> {user.pre_voting_date}</p>
+                                        <p><strong>prev vote to:</strong> {user.pre_vote_to}</p>
+                                        <p><strong>membership: </strong>{user.membership}</p>
+                                        <p><strong>affiliation:</strong> {user.affiliation}</p>
+                                        <p><strong>influence factor:</strong> {user.influence_factor}</p>
+                                        {/* Include other user details */}
+                                    </div>
+                                )}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
+
