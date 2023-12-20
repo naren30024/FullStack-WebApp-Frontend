d58640ecd063c55a86dc7ee21a24355bb8b3e711
naren30024 <126703711+naren30024@users.noreply.github.com>
2023-12-20 19:03:11 +0530

    Update SidenavBar.jsx

--- SidenavBar.jsx
+++ SidenavBar.jsx
@@ -7,81 +7,44 @@ import TopNavBar from './TopNavBar';
 import style from './style.module.css';
 
 export default function SidenavBar() {
-    let [content, setContent] = useState(<ParentHierarchy />);
-
-    const parentHierarchy = () => {
-        setContent(<ParentHierarchy />);
-    };
-
-    const masterData = () => {
-        setContent(<MasterData />);
-    };
-
-    const users = () => {
-        setContent(<Users />);
-    };
+    
+    const contents = [
+        {
+            name:'Parent Hierarchy',
+            link:'/components/ParentHierarchy'
+        },
+        {name:'masterdata',link:'/components/MasterData'},
+        {name:'roles',link:''},
+        {name:'responsibilities',link:''},
+        {name:'users',link:'/components/Users'},
+        {name:'election',link:''},
+        {name:'survey',link:''}
+    ]
 
     return (
         <div className="flex">
-            {/* Top Navigation */}
-            <div className="w-full fixed top-0 z-10">
-                <TopNavBar />
-            </div>
-
-            {/* Side Navigation (Fixed Left) */}
+            
             <div className="w-1/5 lg:w-1/6 h-screen bg-gray-200 fixed left-5 top-20 z-20 hidden md:block">
                 <ul className="mt-20">
-                    <li
+                    {contents.map((content) => (
+                        <li
                         className={`${style.li} cursor-pointer py-2 px-4`}
-                        onClick={parentHierarchy}
+                    
                     >
-                        ParentHierarchy
-                    </li>
-                    <li
-                        className={`${style.li} cursor-pointer py-2 px-4`}
-                        onClick={masterData}
-                    >
-                        MASTERDATA
-                    </li>
-                    <li
-                        className={`${style.li} cursor-pointer py-2 px-4`}
+                        <Link href={content.link}>{content.name}</Link>
                         
-                    >
-                        ROLES
                     </li>
-                    <li
-                        className={`${style.li} cursor-pointer py-2 px-4`}
-                       
-                    >
-                        RESPONSIBILITIES
-                    </li>
-                    <li
-                        className={`${style.li} cursor-pointer py-2 px-4`}
-                        onClick={users}
-                    >
-                        USERS
-                    </li>
-                    <li
-                        className={`${style.li} cursor-pointer py-2 px-4`}
-                        
-                    >
-                        ELECTION
-                    </li>
-                    <li
-                        className={`${style.li} cursor-pointer py-2 px-4`}
-                        
-                    >
-                        SURVEY
-                    </li>
-                    {/* Other list items */}
+                    ))}
+                    
+                    
+                    
+    
+                   
                 </ul>
             </div>
 
-            {/* Content Section */}
-            <div className="ml-70 lg:ml-64 w-full p-8 mt-20 flex justify-center items-center hidden md:block ">
-                {content}
-            </div>
+            
+            
         </div>
     );
 }
-
