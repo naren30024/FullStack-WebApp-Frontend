179398dee2237da627909c649e9ab4d90cc4c595
naren30024 <126703711+naren30024@users.noreply.github.com>
2023-12-20 18:14:52 +0530

    Update SidenavBar.jsx

--- SidenavBar.jsx
+++ SidenavBar.jsx
@@ -1,51 +1,87 @@
-import React, { useState } from 'react'
-//import styles from './sample.module.css'
-import Link from 'next/link'
-import ParentHierarchy from './ParentHierarchy'
-import MasterData from './MasterData'
-import Users from './Users'
-import TopNavBar from './TopNavBar'
+import React, { useState } from 'react';
+import Link from 'next/link';
+import ParentHierarchy from './ParentHierarchy';
+import MasterData from './MasterData';
+import Users from './Users';
+import TopNavBar from './TopNavBar';
 import style from './style.module.css';
 
-
-
 export default function SidenavBar() {
-    let [content, setContent] = useState(<ParentHierarchy/>);
-    
-    const parentHierarchy = () =>{
-        setContent(<ParentHierarchy/>);
-    }
-    const masterData = () =>{
-        setContent(<MasterData/>);
-    }
-    const users = () =>{
-        setContent(<Users/>);
-    }
-    
-  return (
-    <div>
-        <div className={style.fixedtop}>
-            <TopNavBar/>
-        </div>
-        <div style={{display:"flex", marginTop:"50px"}}>
-            <div className={style.fixedleft} style={{marginTop:"90px", background:"skyblue"}}>
-                <ul>
-                    <li className={style.li} style={{cursor:"pointer", padding:"20px"}} onClick={parentHierarchy}>
-                         ParentHierachy
+    let [content, setContent] = useState(<ParentHierarchy />);
+
+    const parentHierarchy = () => {
+        setContent(<ParentHierarchy />);
+    };
+
+    const masterData = () => {
+        setContent(<MasterData />);
+    };
+
+    const users = () => {
+        setContent(<Users />);
+    };
+
+    return (
+        <div className="flex">
+            {/* Top Navigation */}
+            <div className="w-full fixed top-0 z-10">
+                <TopNavBar />
+            </div>
+
+            {/* Side Navigation (Fixed Left) */}
+            <div className="w-1/5 lg:w-1/6 h-screen bg-gray-200 fixed left-5 top-20 z-20 hidden md:block">
+                <ul className="mt-20">
+                    <li
+                        className={`${style.li} cursor-pointer py-2 px-4`}
+                        onClick={parentHierarchy}
+                    >
+                        ParentHierarchy
                     </li>
-                    <li className={style.li} style={{cursor:"pointer", padding:"20px"}} onClick={masterData}>MASTERDATA</li>
-                    <li className={style.li} style={{cursor:"pointer", padding:"20px"}}>ROLES</li>
-                    <li className={style.li} style={{cursor:"pointer", padding:"20px"}}>RESPONSIBILITIES</li>
-                    <li className={style.li} style={{cursor:"pointer", padding:"20px"}} onClick={users}>USERS</li>
-                    <li className={style.li} style={{cursor:"pointer",padding:"20px"}}> ELECTION </li>
-                    <li className={style.li} style={{cursor:"pointer", padding:"20px"}}>SURVEY</li>
+                    <li
+                        className={`${style.li} cursor-pointer py-2 px-4`}
+                        onClick={masterData}
+                    >
+                        MASTERDATA
+                    </li>
+                    <li
+                        className={`${style.li} cursor-pointer py-2 px-4`}
+                        
+                    >
+                        ROLES
+                    </li>
+                    <li
+                        className={`${style.li} cursor-pointer py-2 px-4`}
+                       
+                    >
+                        RESPONSIBILITIES
+                    </li>
+                    <li
+                        className={`${style.li} cursor-pointer py-2 px-4`}
+                        onClick={users}
+                    >
+                        USERS
+                    </li>
+                    <li
+                        className={`${style.li} cursor-pointer py-2 px-4`}
+                        
+                    >
+                        ELECTION
+                    </li>
+                    <li
+                        className={`${style.li} cursor-pointer py-2 px-4`}
+                        
+                    >
+                        SURVEY
+                    </li>
+                    {/* Other list items */}
                 </ul>
             </div>
-            <div className={style.contents}>
+
+            {/* Content Section */}
+            <div className="ml-70 lg:ml-64 w-full p-8 mt-20 flex justify-center items-center hidden md:block ">
                 {content}
             </div>
         </div>
-        
-    </div>
-  )
+    );
 }
+
